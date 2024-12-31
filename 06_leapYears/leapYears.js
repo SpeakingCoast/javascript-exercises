9dcff54c5ef75c2e0284115da36b5c9957e447c9
İrem Ünlü <121850948+SpeakingCoast@users.noreply.github.com>
2024-12-31 07:51:58 +0000

    First 7 problem is solved.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 };
+
+// - use an `if` statement and `&&` to make
+// sure all the conditions are met properly
+
 
 // Do not edit below this line
 module.exports = leapYears;
