3720311e2d592f82881e754e0f2ba6c748dffb63
SpeakingCoast <iremu.unluu@gmail.com>
2025-02-16 13:09:19 +0300

    All exercises are solved.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr[0]) {
+    return arr.reduce((acc, curr) => acc + curr);
+  }
+  else if (!arr[0]) {
+    return 0;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((num1, num2) => num1 * num2);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	var result = 1;
+  for (i = 0; i < b; i++) {
+    result *= a;
+  };
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	var result = 1;
+  for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const year = new Date().getFullYear();
+    return array.reduce((acc, curr) => {
 
-};
-
+        if(!acc.yearOfDeath && !curr.yearOfDeath){if ((year-acc.yearOfBirth) < (year-curr.yearOfBirth)){
+            return curr
+        }}
+        else if(acc.yearOfDeath && !curr.yearOfDeath) {
+            if ((acc.yearOfDeath-acc.yearOfBirth) < (year-curr.yearOfBirth)){
+                return curr
+            }
+        }
+        else if(!acc.yearOfDeath && curr.yearOfDeath){
+            if ((year-acc.yearOfBirth) < (curr.yearOfDeath-curr.yearOfBirth)){
+                return curr
+            }
+        
+        return acc;
+    }
+    
+    else if ((acc.yearOfDeath-acc.yearOfBirth) < (curr.yearOfDeath-curr.yearOfBirth)){
+            return curr
+        }
+        return acc;
+    }
+    )};
 // Do not edit below this line
+
 module.exports = findTheOldest;
